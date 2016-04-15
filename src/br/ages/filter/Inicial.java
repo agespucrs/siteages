72243d84746f7da6f321b53d2a54249ab4c22406
Cassio Trindade <cassiowt@gmail.com>
2016-04-15 13:21:57 -0300

    Inclusão da variável do numero da versão.

---
+++ src/br/ages/filter/Inicial.java
@@ -0,0 +1,59 @@
+package br.ages.filter;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+import br.ages.crud.util.Util;
+
+/**
+ * Servlet Filter implementation class Inicial
+ */
+@WebFilter("/*")
+public class Inicial implements Filter {
+
+	Logger logger = Logger.getLogger("servlet.FileUploadServlet");
+	private Util util;
+    /**
+     * Default constructor. 
+     */
+    public Inicial() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = ((HttpServletRequest) request);
+		HttpSession session = httpRequest.getSession();
+		util = new Util();
+		session.setAttribute("versao", util.getVersion());
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		logger.info(fConfig.getServletContext().getAttributeNames().toString() +" "+ new Date());
+	}
+}
--- src/br/ages/test/Exemplo.java
+++ src/br/ages/test/Exemplo.java
@@ -1,4 +1,4 @@
-package com.ages.test;
+package br.ages.test;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
